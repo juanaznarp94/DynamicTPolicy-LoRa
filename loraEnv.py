c88583014d5792f2de2d7b089f83160a50df6abb
LauraAcostaG <75728304+LauraAcostaG@users.noreply.github.com>
2022-08-02 19:53:36 +0200

    New changes

--- loraEnv.py
+++ loraEnv.py
@@ -16,9 +16,9 @@ from scipy import stats
 from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
 
 TDC = 1 / 100
-Q = 0.051
-QMAX = 3600 * TDC / Q
-Qr = 5
+q = 0.051
+QMAX = 3600 * TDC / q
+Qr = 1 #preguntar
 max_battery_level = 950
 
 class loraEnv(gym.Env):
@@ -28,7 +28,6 @@ class loraEnv(gym.Env):
             return 1
         else:
             return -1
-
 
     def __init__(self, nodes, links):
         super(loraEnv, self).__init__()
@@ -48,61 +47,75 @@ class loraEnv(gym.Env):
         self.E = np.zeros((1, 1))
         self.N = np.zeros((1, 1))
         self.state = [self.Q, self.G, self.E, self.N]
-        #añadir comentario
-        a_1 = [4 / 5, 7, 'a1', -30.2580, 0.2857, 3410, 0.0001778279,590]
-        a_2 = [4 / 5, 8, 'a2', -77.1002, 0.2993, 1841, 0.0000999999,318]
-        a_3 = [4 / 5, 9, 'a3', -244.6424, 0.3223, 1015, 0.0000562341,175]
-        a_4 = [4 / 5, 10, 'a4', -725.9556, 0.3340, 507, 0.0000316227,87]
-        a_5 = [4 / 5, 11, 'a5', -2109.8064, 0.3407, 253, 0.0000177827,43]
-        a_6 = [4 / 5, 12, 'a6', -4452.3653, 0.2217, 127, 0.0000099999,21]
-        a_7 = [4 / 7, 7, 'a7', -105.1966, 0.3746, 2663, 0.0001778279,460]
-        a_8 = [4 / 7, 8, 'a8', -289.8133, 0.3756, 1466, 0.0000999999,253]
-        a_9 = [4 / 7, 9, 'a9', -1114.3312, 0.3969, 816, 0.0000562341,141]
-        a_10 = [4 / 7, 10, 'a10', -4285.4440, 0.4116, 408, 0.0000316227,70]
-        a_11 = [4 / 7, 11, 'a11', -20771.6945, 0.4332, 204, 0.0000177827,35]
-        a_12 = [4 / 7, 12, 'a12', -98658.1166, 0.4485, 102, 0.0000099999,17]
-        self.actions = np.array([a_1, a_2, a_3, a_4, a_5, a_5, a_6, a_7, a_8, a_9, a_10, a_11, a_12])
-        self.txt = np.zeros(self.N)
-        self.sg1 = np.zeros(self.N)
 
-        self.battery_capacity = 950 #mAh 
-        self.bw = 125 #kHz
+        # Definimos los diferentes parámetros de cada configuración
+        a_1 = {'CR': 4 / 5, 'SF': 7, 'ai': 'a1', 'alpha': -30.2580, 'beta': 0.2857, 'TXR': 3410, 'SNR': 0.0001778279,'NMax_Paq': self.N*3410*q/208}
+        a_2 = {'CR': 4 / 5, 'SF': 8, 'ai': 'a2', 'alpha': -77.1002, 'beta': 0.2993, 'TXR': 1841, 'SNR': 0.0000999999,'NMax_Paq': self.N*1841*q/208}
+        a_3 = {'CR': 4 / 5, 'SF': 9, 'ai': 'a3', 'alpha': -244.6424, 'beta': 0.3223, 'TXR': 1015, 'SNR': 0.0000562341,'NMax_Paq': self.N*1015*q/208}
+        a_4 = {'CR': 4 / 5, 'SF': 10, 'ai': 'a4', 'alpha': -725.9556, 'beta': 0.3340, 'TXR': 507, 'SNR': 0.0000316227,'NMax_Paq': self.N*507*q/208}
+        a_5 = {'CR': 4 / 5, 'SF': 11, 'ai': 'a5', 'alpha': -2109.8064, 'beta': 0.3407, 'TXR': 253, 'SNR': 0.0000177827,'NMax_Paq': self.N*253*q/208}
+        a_6 = {'CR': 4 / 5, 'SF': 12, 'ai': 'a6', 'alpha': -4452.3653, 'beta': 0.2217, 'TXR': 127, 'SNR': 0.0000099999,'NMax_Paq': self.N*127*q/208}
+        a_7 = {'CR': 4 / 7, 'SF': 7, 'ai': 'a7', 'alpha': -105.1966, 'beta': 0.3746, 'TXR': 2663, 'SNR': 0.0001778279,'NMax_Paq': self.N*2663*q/208}
+        a_8 = {'CR': 4 / 7, 'SF': 8, 'ai': 'a8', 'alpha': -289.8133, 'beta': 0.3756, 'TXR': 1466, 'SNR': 0.0000999999,'NMax_Paq': self.N*1466*q/208}
+        a_9 = {'CR': 4 / 7, 'SF': 9, 'ai': 'a9', 'alpha': -1114.3312, 'beta': 0.3969, 'TXR': 816, 'SNR': 0.0000562341,'NMax_Paq': self.N*816*q/208}
+        a_10 = {'CR': 4 / 7, 'SF': 10, 'ai': 'a10', 'alpha': -4285.4440, 'beta': 0.4116, 'TXR': 408, 'SNR': 0.0000316227,'NMax_Paq': self.N*408*q/208}
+        a_11 = {'CR': 4 / 7, 'SF': 11, 'ai': 'a11', 'alpha': -20771.6945, 'beta': 0.4332, 'TXR': 204, 'SNR': 0.0000177827,'NMax_Paq': self.N*204*q/208}
+        a_12 = {'CR':4 / 7, 'SF': 12, 'ai': 'a12', 'alpha': -98658.1166, 'beta': 0.4485, 'TXR': 102, 'SNR': 0.0000099999,'NMax_Paq': self.N*102*q/208}
+        # Defino array con las diferentes configuraciones
+        self.actions = np.array[a_1, a_2, a_3, a_4, a_5, a_5, a_6, a_7, a_8, a_9, a_10, a_11, a_12]
+        # Defino arrays que voy a necesitar para calcular el pdr
+        self.txt = np.zeros(self.N)  # Array donde voy a almacenar el sumatorio de las transmisiones realizadas de los nodos externos
+        self.sg1 = np.zeros(self.N)  # Array donde voy a almacenar el sumatorio de las transmisiones intentadas por los nodos externos
 
+        self.battery_capacity = 0.950  # Capacidad de la batería en mAh
+        self.bw = 125000  # Ancho de banda en Hz
+        print(2)
     def step(self, action):
         # Called to take an action with the environment, it returns the next observation,
         # the immediate reward, whether the episode is over and additional information
-        reward = -100
+        reward = -1
 
         if self.Q == self.QMAX:
-            tx = np.zeros(self.N) #Añadir coment
+            tx = np.zeros(self.N)  # En este array definimos las transmisiones que se llevan a cabo de los nodos externos
 
             # Array 1xN para generar eventos Bernoulli
             p = 0.5
             bernoulli = stats.bernoulli(p)
-            g1 = bernoulli.rvs(p, size=self.N)
+            g1 = bernoulli.rvs(p, size=self.N)  # Ej: [1 0 0 0 1 1 0 1 1 1 0] (1's quieren tx 0's no)
 
-            #Creamos otro array 1xN random entre 0,1 y 2 --> Prioridades
-            g2 = np.random.randint(low=0, high=2, size=self.N, dtype=int)
+            # Creamos otro array 1xN random entre 0,1 y 2 --> Prioridades
+            g2 = np.random.randint(low=0, high=2, size=self.N, dtype=int) # Ej: [1 0 0 2 1 1 0 2 0 1 0]
 
-            #Multiplicamos los arrays para dar prioridad a los vecinos
-            G_N = np.dot(g1, g2) #Prioridad nodos externos
+            # Multiplicamos los arrays para dar prioridad a los vecinos
+            G_N = np.dot(g1, g2) #Prioridad nodos externos. Ej: [1 0 0 0 1 1 0 2 0 1 0]
 
-            config = self.actions[action] #configuracion q elegimos
+            config = self.actions[action] # Configuracion que elegimos que es la acción
 
-            sg1 = np.add(self.sg1,g1)
+            sg1 = np.add(self.sg1, g1) # Sumamos a sg1 g1, donde tengo los nodos que quieren transmitir en esa iteración para ir acumulando
+            # Ej: [0 0 0 0 0 0 0 0 0 0 0] + [1 0 0 0 1 1 0 1 1 1 0] = [1 0 0 0 1 1 0 1 1 1 0]
 
+            # Definimos el número de paquetes de los nodos externos que finalmente vamos a transmitir
+            # num_paq es el número de paquetes que finalmente transmites otra y  máx paq el número máximo de paquetes que podrías transmitir
             if (config[7]) >= sum(g1):
-                num_paq = sum(g1) #Numero de paquetes de nodos ext
-                tx = g1
+                num_paq = sum(g1) # En caso de que el num máx de paq permitido sea >= al que quiero transmitir, transmito todos los que tengo en g1
+                tx = g1 # En ese array guardo las transmisiones que finalmente se realizan de los nodos externos
+                #En el caso del ej tendríamos esta opción y tx = [1 0 0 0 1 1 0 1 1 1 0]
             else:
-                num_paq = config[7]
-                index = np.min(G_N[np.nonzero(G_N)]).index()
+                num_paq = config[7] # En caso de que quieran tx más nodos que paq permitidos, se transmite el máximo posible y se elimina un paquete
+                index = np.min(G_N[np.nonzero(G_N)]).index() # Aquí eliminamos el paquete con menor prioridad
                 np.delete(g1, index)
                 tx = g1
 
-            txt = np.add(self.txt,tx)
-            pdr = np.divide(txt,sg1)
-            #hacer la media d todos los elementos
+            txt = np.add(self.txt, tx) # Vamos sumando las transmisiones que finalmente se realizan
+            # Ej: [0 0 0 0 0 0 0 0 0 0 0] + [1 0 0 0 1 1 0 1 1 1 0] = [1 0 0 0 1 1 0 1 1 1 0]
+
+            pdr = np.divide(txt, sg1) # Calculamos el pdr diviendo los arrays txt y sg1
+            # Ej: [1 0 0 0 1 1 0 1 1 1 0] / [1 0 0 0 1 1 0 1 1 1 0] = [1 0 0 0 1 1 0 1 1 1 0]
+            # Hacemos la media d todos los elementos
+            sumatorio = 0
+            for i in pdr:
+                sumatorio += pdr[i]
+            pdr = sumatorio / len(pdr)  # 6/11 = 0.545454
 
             #else:
             #   raise ValueError("Received invalid action={} which is not part of the action space".format(action))
@@ -124,23 +137,23 @@ class loraEnv(gym.Env):
                 h = 1
                 de = 0
                 n_p = 0 #lo incluimos en el payload
-                n_payload = 8 + max((8*pl - 4* sf + 16 +28 -20*h)/(config[1] - 2*de),0)
-                t_symbol = pow(2, config[4])/ self.bw
+                n_payload = 8 + max((8 * pl - 4 * sf + 16 + 28 - 20 * h)/(config[1] - 2 * de), 0)
+                t_symbol = pow(2, config[4]) / self.bw
                 t_preamble = (4.25 + n_p) * t_symbol
                 t_payload = n_payload * t_symbol
 
                 p_cons = 412.5
 
-                e_bit = (p_cons * (n_payload + n_p + 4.25) * t_symbol) / (8*pl)
+                e_bit = (p_cons * (n_payload + n_p + 4.25) * t_symbol) / (8 * pl)
                 num_bits = 208*num_paq
 
                 self.battery_capacity = self.battery_capacity - e_bit * num_bits
 
                 ## REWARD !!
-                reward = 10*self.heaviside(prr,0.8) + 10*self.heaviside(pdr,0.8) - self.heaviside(e_bit*num_bits,)
+                reward = 10 * self.heaviside(prr, 0.8) + 10 * self.heaviside(pdr, 0.8) - self.heaviside(e_bit*num_bits, 0.0042)
 
             else:
-                reward = -400
+                reward = -1
 
 
             #reward - energia, prr, pdr, y prioridad
@@ -164,7 +177,7 @@ class loraEnv(gym.Env):
     def close(self):
         pass
 
-
+""" 
 gw = (0,0) # Gateway o Base Station
 p0 = (1000, 2000) # Nodo 0
 p1 = (2000, 2000) # Nodo 1
@@ -194,20 +207,20 @@ buildings = [
     Polygon([(1700, 4500), (1700, 5000), (4500, 5000), (4500, 4500)])
 ]
 
-"""
+
 TAREA 0: 
 Pasar todo a Km (Opcional)
-"""
+
 #nodes_km = []
 #for i, n in enumerate(nodes):
 #    nodes_km[i] = nodes[i] /
 
 #buildings_km = []
 
-"""
+
 TAREA 1: 
 Implementar función compute_links()
-"""
+
 #dados los nodos y los edificios calcular si entre cada par de nodos hay un enlace o no
 #rcs = math.gamma(m + 1.0 / beta) / (math.gamma(m) * (S * A * m / p) ** (1.0 / beta))
     # Si dos nodos NO tienen ningún edificio en medio -> (m = 2, beta = 2), calcular rcs y ver si están en rango o no
@@ -256,10 +269,10 @@ def compute_links(nodes, buildings):
                     links[i][j] = 1
     return links
 
-"""
+
 TAREA 2: 
 Añadir links como parámetro de entrada a función plot_map() y representar enlaces con líneas entre dos puntos o nodos
-"""
+
 def plot_map(nodes, buildings, links):
     fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(7, 5))
     # Plot nodes including GW
@@ -296,10 +309,10 @@ def plot_map(nodes, buildings, links):
     plt.savefig('map.png', dpi=300)
     plt.show()
 
-"""
+
 TAREA 3:
 Implementar la función compute_paths()
-"""
+
 def compute_paths(nodes, links):
     paths = []
     #
@@ -309,4 +322,5 @@ def compute_paths(nodes, links):
     return paths
 
 compute_links(nodes, buildings)
-plot_map(nodes, buildings, links)
+#plot_map(nodes, buildings, links)
+"""
